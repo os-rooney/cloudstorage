97aa6f546918223a8b09f6565afac5a731c9708c
Roosman <dev@ronios.de>
2023-10-07 21:52:51 +0200

    file mapper methods

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/FileMapper.java
@@ -0,0 +1,29 @@
+package com.udacity.jwdnd.course1.cloudstorage.mapper;
+
+import com.udacity.jwdnd.course1.cloudstorage.entity.File;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface FileMapper {
+
+    @Select("SELECT * FROM FILES WHERE userid = #{userId}")
+    List<File> getFiles(Integer userId);
+
+    @Select("SELECT * FROM FILES WHERE filename=#{filename} AND userid=#{userId}")
+    File getFileByName(String filename, Integer userId);
+
+    @Insert("INSERT INTO FILES (filename, contenttype, filesize, userid, filedata) VALUES (#{filename}, #{contentType}, #{fileSize}, #{userId}, #{fileData})")
+    @Options(useGeneratedKeys = true, keyProperty = "fileId")
+    void saveFile(File file);
+
+    @Delete("DELETE FROM FILES WHERE fileid = #{fileId}")
+    void deleteFileById(Integer fileId);
+
+}
+
+
+
+
+

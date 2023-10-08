6dc08fed217249a02292dff74658313ae84dcaaf
Roosman <dev@ronios.de>
2023-10-08 09:42:31 +0200

    Notes service class

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/NoteService.java
@@ -0,0 +1,28 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.entity.Note;
+import com.udacity.jwdnd.course1.cloudstorage.mapper.NoteMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NoteService {
+
+    private final NoteMapper noteMapper;
+
+    NoteService(NoteMapper noteMapper) {
+        this.noteMapper = noteMapper;
+    }
+
+    public List<Note> getNotes(Integer userId) {
+        return noteMapper.getNotes(userId);
+    }
+
+    public void saveNote(Note note) {
+        noteMapper.saveNote(note);
+    }
+    public void deleteNoteById(Integer noteId, Integer userId) {
+        noteMapper.deleteFileById(noteId, userId);
+    }
+}

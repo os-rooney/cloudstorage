b386cabc11482ae8aedccb45a1a7252e37f37c0b
Roosman <dev@ronios.de>
2023-10-08 15:37:05 +0200

    extend the home controller to decrypt the passwords

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/MainController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/MainController.java
@@ -1,14 +1,14 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.entity.Credential;
 import com.udacity.jwdnd.course1.cloudstorage.entity.User;
-import com.udacity.jwdnd.course1.cloudstorage.services.CredentialService;
-import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
-import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
-import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import com.udacity.jwdnd.course1.cloudstorage.services.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -17,12 +17,18 @@ public class MainController {
     private final FileService fileService;
     private final NoteService noteService;
     private final CredentialService credentialService;
+    private final EncryptionService encryptionService;
 
-    public MainController(FileService fileService, UserService userService, NoteService noteService, CredentialService credentialService) {
+    public MainController(FileService fileService,
+                          UserService userService,
+                          NoteService noteService,
+                          CredentialService credentialService,
+                          EncryptionService encryptionService) {
         this.fileService = fileService;
         this.userService = userService;
         this.noteService = noteService;
         this.credentialService = credentialService;
+        this.encryptionService = encryptionService;
     }
 
     @GetMapping("/home")
@@ -30,7 +36,11 @@ public class MainController {
         User user = userService.getUserFromAuthentication(authentication);
         model.addAttribute("files", fileService.getFiles(user.getUserId()));
         model.addAttribute("notes", noteService.getNotes(user.getUserId()));
-        model.addAttribute("credentials", credentialService.getCredentials(user.getUserId()));
+        List<Credential> credentials = credentialService.getCredentials(user.getUserId());
+        for(Credential credential : credentials) {
+            credential.setPassword(encryptionService.decryptValue(credential.getPassword(), credential.getKey()));
+        }
+        model.addAttribute("credentials", credentials);
         return "home";
     }
 }

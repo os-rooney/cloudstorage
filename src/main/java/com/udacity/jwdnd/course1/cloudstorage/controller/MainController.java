c4991a70fee4223c13fa34951560b2a165b57e4c
Roosman <dev@ronios.de>
2023-10-08 09:41:29 +0200

    adjsut the home controller for notes

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/MainController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/MainController.java
@@ -2,6 +2,7 @@ package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import com.udacity.jwdnd.course1.cloudstorage.entity.User;
 import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
+import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -13,16 +14,19 @@ public class MainController {
 
     private final UserService userService;
     private final FileService fileService;
+    private final NoteService noteService;
 
-    public MainController(FileService fileService, UserService userService) {
+    public MainController(FileService fileService, UserService userService, NoteService noteService) {
         this.fileService = fileService;
         this.userService = userService;
+        this.noteService = noteService;
     }
 
     @GetMapping("/home")
     public String getHomePage(Authentication authentication, Model model) {
-        User user = userService.getUser(authentication.getName());
+        User user = userService.getUserFromAuthentication(authentication);
         model.addAttribute("files", fileService.getFiles(user.getUserId()));
+        model.addAttribute("notes", noteService.getNotes(user.getUserId()));
         return "home";
     }
 }
